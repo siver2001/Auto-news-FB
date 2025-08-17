11920c0ff6a9789e9746212fdab17e5b5e558cbe
siver2001 <longro0211vn147@gmail.com>
2025-08-17 18:55:16 +0700

    Deploy web

--- renderer/scripts/renderer.js
+++ renderer/scripts/renderer.js
@@ -309,7 +309,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     
     // Sử dụng WebSocket để nhận thông tin cập nhật theo thời gian thực
-    const ws = new WebSocket(`ws://localhost:${window.location.port || '3000'}/ws`);
+    const ws = new WebSocket(`ws://${window.location.host}/ws`);
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
       handleBackendLog(data);
